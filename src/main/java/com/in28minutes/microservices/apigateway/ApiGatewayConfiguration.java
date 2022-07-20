9e4dacb8052201a1ad71e383811ad2cd416f3ba7
Michael <michael.chan@wclsolution.com>
2022-07-20 09:20:47 +0800

    Initial commit

---
+++ src/main/java/com/in28minutes/microservices/apigateway/ApiGatewayConfiguration.java
@@ -0,0 +1,21 @@
+package com.in28minutes.microservices.apigateway;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApiGatewayConfiguration {
+	@Bean
+	public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route(p -> p.path("/currency-conversion-new/**")
+						.filters(f -> f.rewritePath(
+								"/currency-conversion-new/(?<segment>.*)",
+								"/currency-conversion-feign/${segment}"))
+						.uri("lb://currency-conversion"))
+				.route(p -> p.path("/a/**").uri("lb://a"))
+				.build();
+	}
+}
